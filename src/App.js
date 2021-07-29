6872dd4200089ff67868af29aadc7478effe8748
Shubh-RAZ <66317664+Shubh-RAZ@users.noreply.github.com>
2021-07-29 18:36:14 +0530

    animation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Homepage from './Components/Homepage/Homepage';
+import Animate from './Components/Animate/Animate';
 import { Route } from 'react-router';
 import { Switch } from 'react-router';
 
@@ -13,6 +14,7 @@ function App() {
         <Route path="/reach" exact component={Homepage}></Route>
         <Route path="/bout" exact component={Homepage}></Route>
         <Route path="/projects" exact component={Homepage}></Route>
+        <Route path="/animate" exact component={Animate} ></Route>
       </Switch>
     </div>
   );
--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -1,6 +1,6 @@
 import React, { useState , useEffect} from 'react';
 import SwipeableViews from 'react-swipeable-views';
-import Corals from '../../Assets/corals.'
+import Coralorange from '../../Assets/coralorange.js'
 import Fishorange from '../../Assets/fishorange'
 import Fishyellow from '../../Assets/fishyellow'
 import Sealion from '../../Assets/sealion';
@@ -9,6 +9,16 @@ import Navbar from '../Navbar/Navbar';
 import './Homepage.css'
 import Starfish from '../../Assets/Starfish';
 import Shark from '../../Assets/Shark';
+import Goliath from '../../Assets/goliath';
+import Turtle from '../../Assets/turtle';
+import Fishsky from '../../Assets/fishsky';
+import Fishskyyellow from '../../Assets/fishskyyellow';
+import Fishred from '../../Assets/fishred';
+import Coral2 from '../../Assets/coral2';
+import Animatecorals from '../../Assets/animate-corals';
+import Cactus from '../../Assets/cactus.js';
+import Coralred from '../../Assets/coral-red.js';
+import Canvas from '../../Assets/Canvas.js';
 const Homepage = () => {
 
     const [ scrollTop , setScrollTop ] = useState(0)
@@ -19,6 +29,13 @@ const Homepage = () => {
     const [ transformStarFish , setStarFish ] = useState()
     const [ scrolled , setScrolled ] = useState(false)
     const [ index , setIndex ] = useState(0)
+    const [ transformOpacity , setOpacity ] = useState()
+    const [ transformOpacityFrontTitle , setOpacityFrontTitle ] = useState()
+    const [ transformOpacityFrontDesc , setOpacityFrontDesc ] = useState()
+    const [ transformOpacityWhatTitle , setOpacityWhatTitle ] = useState()
+    const [ transformOpacityWhatDesc , setOpacityWhatDesc ] = useState()
+    const [ transformOpacityWhoTitle , setOpacityWhoTitle ] = useState()
+    const [ transformOpacityWhoDesc , setOpacityWhoDesc ] = useState()
 
     const myRef = React.createRef()
 
@@ -37,6 +54,19 @@ const Homepage = () => {
           function handleResize() {
             setWindowDimensions(getWindowDimensions());
           }
+
+          setOpacityFrontTitle({
+            animation:'fade 5s linear',
+            transition:'all 5s',
+            opacity:'1'
+        })
+
+          setOpacityFrontDesc({
+            animation:'fade 10s linear',
+            transition:'all 10s',
+            opacity:'1'
+        })
+        
       
           window.addEventListener('resize', handleResize);
           return () => window.removeEventListener('resize', handleResize);
@@ -48,6 +78,8 @@ const Homepage = () => {
       const { height, width } = useWindowDimensions();
 
     const onScroll = () => {
+
+     
         const attainedscrollTop = myRef.current.scrollTop
         console.log(scrollTop)
         setScrollTop(attainedscrollTop)
@@ -69,12 +101,44 @@ const Homepage = () => {
                 transform:`translateX(-${attainedscrollTop - height}px)`,
                 transalate:`2s linear ease-in-out`
             })
+
+            setOpacityWhatTitle({
+                animation:'fade 5s linear',
+                transition:'all 5s',
+                opacity:'1'
+            })
+    
+              setOpacityWhatDesc({
+                animation:'fade 10s linear',
+                transition:'all 10s',
+                opacity:'1'
+            })
         }
 
         if( attainedscrollTop > 2*height){
             setStarFish({
                 transform:`translateX(-${attainedscrollTop - 2*height}px)`,
-                transalate:`2s linear ease-in-out`
+                transition:`2s linear ease-in-out`
+            })
+
+            setOpacityWhoTitle({
+                animation:'fade 5s linear',
+                transition:'all 5s',
+                opacity:'1'
+            })
+    
+              setOpacityWhoDesc({
+                animation:'fade 10s linear',
+                transition:'all 10s',
+                opacity:'1'
+            })
+        }
+
+        if( attainedscrollTop > 3* height) {
+            setOpacity({
+                animation:'fade 5s linear',
+                transition:'all 5s',
+                opacity:'1'
             })
         }
      
@@ -92,7 +156,8 @@ const Homepage = () => {
     const handleChangeIndex = index => {
        setIndex(index)
       };
-    
+
+ 
     return ( 
         <div className="homepage"  ref={myRef} onScroll={onScroll}>
             <div className="side-line">
@@ -112,16 +177,16 @@ const Homepage = () => {
             </div>
 
             <div className="slide-content">
-                <div className="corals" ><Corals></Corals> </div>
+                <div className="corals" ><Animatecorals></Animatecorals> </div>
                 <div className="fish-orange" style={transformOrange}><Fishorange></Fishorange></div>
                 <div className="fish-yellow" style={transformYellow}><Fishyellow></Fishyellow></div>
                 <div className="shark"><Shark></Shark></div>
                 <div className="content-front">
                     <div className="inside-cont">
-                        <div className="Logo-1">Web</div>
-                        <div className="Logo-2">The Builder</div>
-                        <div className="desc-1">We Build Interactive</div>
-                        <div className="desc-2">Websites for our beloved user </div>
+                        <div className="Logo-1" style={transformOpacityFrontTitle} >Web</div>
+                        <div className="Logo-2" style={transformOpacityFrontTitle}>The Builder</div>
+                        <div className="desc-1" style={transformOpacityFrontDesc}>We Build Interactive</div>
+                        <div className="desc-2" style={transformOpacityFrontDesc}>Websites for our beloved user </div>
                     </div>
                 </div>
       
@@ -131,8 +196,8 @@ const Homepage = () => {
                 <div className="allien" style={transformAlien}><Allien></Allien></div>
                 <div className="content-what-we-do">
                     <div className="inside-what">
-                        <div className="topic-heading">What We Do</div>
-                        <div className="topic-desc">We turn your bussiness into a piece of art which is being
+                        <div className="topic-heading" style={transformOpacityWhatTitle}>What We Do</div>
+                        <div className="topic-desc" style={transformOpacityWhatDesc}>We turn your bussiness into a piece of art which is being
 loved by every other in the marketplace . We help people to 
 expand thier work and realise its trur potential </div>
                     </div>
@@ -142,10 +207,11 @@ expand thier work and realise its trur potential </div>
 
             <div className="slide-content-3">
             <div className="star-fish" style={transformStarFish}><Starfish></Starfish></div>
+            <div className="goliath" style={transformStarFish}><Goliath></Goliath></div>
                 <div className="content-what-we-do">
                     <div className="inside-what">
-                        <div className="topic-heading">Who Are We</div>
-                        <div className="topic-desc">We are a bunch of students from IIT Tirupati with  web 
+                        <div className="topic-heading" style={transformOpacityWhoTitle}>Who Are We</div>
+                        <div className="topic-desc" style={transformOpacityWhoDesc}>We are a bunch of students from IIT Tirupati with  web 
 development experience of more than 2 yrs .We are a bunch of students from IIT Tirupati with  web 
 development experience of more than 2 yrs . </div>
                     </div>
@@ -198,6 +264,23 @@ development experience of more than 2 yrs .</div>
         </SwipeableViews>
                     </div>
                     </div>
+            </div>
+
+            <div className="slide-content-last">
+                 {/* <div className="coral-orange"><Coralorange></Coralorange></div> 
+                 <div className="coral-red"><Coralred></Coralred></div>
+                 <div className="cactus"><Cactus></Cactus></div>
+                <div className="fish-sky"><Fishsky></Fishsky></div>
+                <div className="fish-sky-yellow"><Fishskyyellow></Fishskyyellow></div>
+                <div className="fish-red"><Fishred></Fishred></div>
+                <div className="turtle"><Turtle></Turtle></div>
+                <div className="corals-left"><Coral2></Coral2></div> */}
+                <div className="canvas"><Canvas></Canvas></div>
+
+                <div className="warning-content" style={transformOpacity}>
+                Thats Too Deep !!
+                <br></br> Pressure is low out here get yourself to shore
+                </div>
             </div>
 
         </div>
